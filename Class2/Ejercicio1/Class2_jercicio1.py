f21e5c8cdd93dc9d652fd54a07153b5f3ba41f6f
krzkraken <cristhian.mirandas@gmail.com>
2022-08-29 22:37:23 -0500

    Ejercicio1 Clase 2

---
+++ Class2/Ejercicio1/Class2_jercicio1.py
@@ -0,0 +1,8 @@
+from traceback import print_tb
+
+
+txt = "Hola, soy una cadena de texto :)"
+tipo = type(txt)
+
+print(txt)
+print(tipo)
--- class1_ejercice1.py
+++
@@ -1,3 +0,0 @@
-print("Hola, soy Cristhian")
-print("Estoy empezando el curso de Python")
-print("Espero aprender mucho")
