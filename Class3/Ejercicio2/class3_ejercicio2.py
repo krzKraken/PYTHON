ffe89184f84a89d08f8c622fee88541e221409c2
krzkraken <cristhian.mirandas@gmail.com>
2022-08-30 01:49:42 -0500

    Class3_ejercicio2

--- Class3/Ejercicio2/class3_ejercicio2.py
+++ Class3/Ejercicio2/class3_ejercicio2.py
@@ -1,19 +1,27 @@
+
 def mensajeError():
     print("Error de datos... terminando Programa")
 
 
+def validarValor(valor):
+    try:
+        float(valor)
+        return True
+    except:
+        return False
+
+
 print("Ingrese su peso en Kilogramos [Kg]: ")
 peso = input()
-if (peso.isnumeric() and int(peso) > 0):
-    print("ingrese su altura en metros [cm]: ")
+if (float(peso) > 0 and validarValor(peso)):
+    print("ingrese su altura en metros [m]: ")
     altura = input()
-    if (altura.isnumeric() and int(altura) > 0):
-        print("Altura: " + altura + " , Peso: " + peso)
+    if (float(altura) > 0 and validarValor(altura)):
+        imc = float(peso) / (float(altura)**2)
+        imcRedondeado = imc.__round__(2)
+        print("IMC: " + str(imcRedondeado))
+
     else:
         mensajeError()
 else:
     mensajeError()
-
-imc = float(peso) / ((float(altura)/100)**2)
-imcRedondeado = imc.__round__(2)
-print(imcRedondeado)
