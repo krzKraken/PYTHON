0bd84b8e5fd6790cc33af783470a26f6840c9798
krzkraken <cristhian.mirandas@gmail.com>
2022-08-30 19:42:45 -0500

    class4_ejercicio3

--- Class4/Ejercicio3/class4_ejercicio3.py
+++ Class4/Ejercicio3/class4_ejercicio3.py
@@ -1,17 +1,16 @@
 # Escribe un programa que sea capaz de mostrar los números del 1 al 100 en orden inverso.
-print("Lista del 1 al 100")
-lista = [1, 2, 3, 4, 5, 6]
-print(lista)
-lista.reverse()
-print(lista)
-
 lista100 = []
 for i in range(1, 101):
     lista100.append(i)
-print(lista100)
+print("Lista creada --> " + str(lista100) + "\n")
+# Con Metodo
+listaInversa = sorted(lista100, reverse=True)
+print("Lista invertida con metodo --> " + str(listaInversa) + "\n")
 
-poss = 0
-for i in range(len(lista100)):
-    if (i == 0):
-        lista100.append(lista100[lista100[0]])
-print(lista100)
+# Sin Metodos
+listaInversa2 = []
+i = len(lista100)
+while i > 0:
+    listaInversa2.append(i)
+    i -= 1
+print("Lista creada:" + str(listaInversa2) + "\n")
