6d3bf4779832343dd78f808a319c5cebaacaacec
krzkraken <cristhian.mirandas@gmail.com>
2022-09-04 16:51:05 -0500

    class8_ejercicio2

---
+++ Class8/Ejercicio2/Class8_ejercicio2.py
@@ -0,0 +1,53 @@
+# En este segundo ejercicio, tendréis que crear un archivo py y dentro crearéis una clase Vehículo, haréis un objeto de ella, lo guardaréis en un archivo y luego lo cargamos.
+
+from select import select
+from turtle import color
+
+
+class Vehiculo:
+    color = ''
+    puertas = 0
+    velocidad = 0
+    cilindrada = 0
+
+    def __init__(self, puertas, velocidad, color, cilindrada):
+        self.puertas = puertas
+        self.velocidad = velocidad
+        self.color = color
+        self.cilindrada = cilindrada
+
+    def imprimeVehiculo(self):
+        detalle = f'''El Vehiculo Tiene las siguientes propiedades:
+Color: {self.color}
+Puertas: {self.puertas}
+velocidad: {self.velocidad}
+cilindrada: {self.cilindrada}'''
+
+        return detalle
+
+
+def guardarFichero(txt):
+    print("Fichero guardado.... con nombre: fichero.txt")
+    f = open("fichero.txt", 'w')
+    f.writelines(txt)
+    f.close()
+
+
+def leerFichero():
+    print("Leyendo fichero.txt....")
+    f = open("fichero.txt", 'r')
+    dato = f.read()
+    f.close()
+    return dato
+
+
+def main():
+    miVehiculo = Vehiculo(color='rojo', puertas=5,
+                          velocidad=100, cilindrada=2000)
+    guardarFichero(miVehiculo.imprimeVehiculo())
+    datos = leerFichero()
+    print(datos)
+
+
+if __name__ == "__main__":
+    main()
