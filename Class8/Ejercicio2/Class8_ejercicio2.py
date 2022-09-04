c3f5fcca27cea2e60d558f37cba8a224973aa051
krzkraken <cristhian.mirandas@gmail.com>
2022-09-04 16:52:00 -0500

    Class8_ejercicio2

--- Class8/Ejercicio2/Class8_ejercicio2.py
+++ Class8/Ejercicio2/Class8_ejercicio2.py
@@ -1,9 +1,4 @@
 # En este segundo ejercicio, tendréis que crear un archivo py y dentro crearéis una clase Vehículo, haréis un objeto de ella, lo guardaréis en un archivo y luego lo cargamos.
-
-from select import select
-from turtle import color
-
-
 class Vehiculo:
     color = ''
     puertas = 0
